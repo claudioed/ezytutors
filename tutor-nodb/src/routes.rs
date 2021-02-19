43186164281df8691dc74529bd0a0d0984700249
claudioed <claudioed.oliveira@gmail.com>
2021-02-19 08:28:05 -0300

    get course details

--- tutor-nodb/src/routes.rs
+++ tutor-nodb/src/routes.rs
@@ -9,5 +9,6 @@ pub fn general_routes(cfg: &mut web::ServiceConfig){
 pub fn course_routes(cfg: &mut web::ServiceConfig){
     cfg.service(web::scope("/courses")
                     .route("/", web::post().to(new_course))
-                    .route("/{user_id}", web::get().to(get_courses_for_tutor)), );
+                    .route("/{user_id}", web::get().to(get_courses_for_tutor))
+                    .route("/{user_id}/{course_id}", web::get().to(get_course_detail)), );
 }
